652aae00127d4de2fd91f4a4734065c17e22c17e
MattuW4 <>
2023-07-07 21:27:52 +0000

    Delete non modal JS code to refresh script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 //Variable declarations
-const selectionButtons = document.querySelectorAll("[data-selection]");
-const CHOICES = [{
+const modal = document.getElementById("my-modal");
+const openModal = document.getElementById("open-modal-button");
+const closeModal = document.querySelector(".return-button");
+
+
+const choices = [{
     name: 'rock',
     image: './assets/images/Rock.png',
     beats: 'scissors'
@@ -17,10 +21,6 @@ const CHOICES = [{
 }
 ];
 
-const modal = document.getElementById("my-modal");
-const openModal = document.getElementById("open-modal-button");
-const closeModal = document.querySelector(".return-button");
-
 // Modal open and hide 
 openModal.addEventListener("click", showModal);
 closeModal.addEventListener("click", hideModal);
@@ -33,41 +33,4 @@ function showModal() {
 /** Hide modal function */
 function hideModal() {
     modal.style.display = "none";
-}
-
-// Informed by the Code Institute Love Maths project
-// Wait for the DOM to finish loading before running game
-// Get button elements and add event listeners to them
-
-
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener("click", e => {
-        const selectionName = selectionButton.dataset.selection;
-        const selection = CHOICES.find(selection => selection.name === selectionName);
-        makeSelection(selection);
-    });
-});
-
-function makeSelection(selection) {
-    const computerSelection = randomSelection();
-    const yourWinner = isWinner(selection, computerSelection);
-    const computerWinner = isWinner(computerSelection, selection);
-    console.log(selection);
-
-
-    addSelectionResult(computerSelection, computerWinner);
-    addSelectionResult(selection, yourWinner);
-}
-
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * CHOICES.length);
-    return CHOICES[randomIndex];
-}
-
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name;
-}
-
-function addSelectionResult(selection, winner) {
-
 }

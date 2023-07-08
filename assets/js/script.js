cf6b4fb34dce45da5a70af5967e17a2a2b80d38e
MattuW4 <>
2023-07-08 18:54:00 +0000

    Git add button listeners JS, user selection JS, computer random generated selection JS, run game function JS, determine winner code JS, round increment JS, computer and user score increment JS, ID and Class tags in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-//Variable declarations
-const modal = document.getElementById("my-modal");
-const openModal = document.getElementById("open-modal-button");
-const closeModal = document.querySelector(".return-button");
-
 const choices = [{
     name: 'rock',
     image: './assets/images/Rock.png',
@@ -20,6 +15,13 @@ const choices = [{
 }
 ];
 
+let playRound = document.querySelector(".roundNumber");
+
+//Variable declarations
+const modal = document.getElementById("my-modal");
+const openModal = document.getElementById("open-modal-button");
+const closeModal = document.querySelector(".return-button");
+
 /**  Modal open and hide */
 openModal.addEventListener("click", showModal);
 closeModal.addEventListener("click", hideModal);
@@ -34,3 +36,123 @@ function hideModal() {
     modal.style.display = "none";
 }
 // Main game 
+
+const buttons = document.querySelectorAll(".player-weapon-choice-button");
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let userChoice = this.getAttribute("data-choice");
+        let userOption = choices.find(choice => choice.name === userChoice);
+        console.log(userChoice);
+
+        runGame(userOption);
+    });
+}
+
+function computerRandomChoice() {
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(randomChoice.name);
+    return randomChoice;
+}
+
+function runGame(userOption) {
+    let computerOption = computerRandomChoice();
+    let computerSelection = computerOption.name;
+    let userSelection = userOption.name;
+    console.log(userOption, computerOption);
+
+    if (userSelection === computerSelection) {
+        drawResult(userSelection, computerSelection);
+    } else {
+        let userWins = whoWins(userOption, computerSelection);
+
+        if (userWins) {
+            winnerUser(userSelection, computerSelection);
+            scoreUser();
+        } else {
+            winnerComputer(userSelection, computerSelection);
+            scoreComputer();
+        }
+    }
+    roundGame(userSelection, computerSelection);
+}
+
+function whoWins(selection, nameSelected) {
+    let options = selection.beats;
+    for (let i = 0; i < options.length; i++) {
+        let option = options[i];
+        if (option === nameSelected) {
+            return true;
+        }
+    }
+}
+
+function drawResult(user, computer) {
+    arenaVictorOutput.innerText = "draw";
+}
+
+function roundGame(user, computer) {
+    if (user && computer) {
+        let incrementRound = parseInt(roundNumber.innerText);
+        roundNumber.innerText = ++incrementRound;
+
+        if (incrementRound === 5) {
+            document.getElementById(roundNumber).innerText = "Game over";
+
+        }
+    }
+}
+
+function computerScore() {
+    let incrementComputerScore = parseInt(computerScore.innerText);
+    computerScore.innerText = ++incrementComputerScore;
+}
+
+function userScore() {
+    let incrementuserScore = parseInt(userScore.innerText);
+    userScore.innerText = ++incrementuserScore;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

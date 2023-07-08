9828e1784edb9c4851d5bca16ffb2d9fe6703e9d
MattuW4 <>
2023-07-08 14:12:20 +0000

    Delete button selection, user choice and computer choice js code to refresh script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ const modal = document.getElementById("my-modal");
 const openModal = document.getElementById("open-modal-button");
 const closeModal = document.querySelector(".return-button");
 
-
 const choices = [{
     name: 'rock',
     image: './assets/images/Rock.png',
@@ -21,7 +20,7 @@ const choices = [{
 }
 ];
 
-// Modal open and hide 
+/**  Modal open and hide */
 openModal.addEventListener("click", showModal);
 closeModal.addEventListener("click", hideModal);
 
@@ -34,30 +33,4 @@ function showModal() {
 function hideModal() {
     modal.style.display = "none";
 }
-// Informed by Math Project, Code Institue and Web Dev Simplified Tutorial
-// Wait for the DOM to load before running the game
-// Get the game section button elements and add event listeners to them
-document.addEventListener('DOMContentLoaded', function () {
-    let gameSectionButtons = document.querySelectorAll('.player-weapon-choice-button');
-
-    for (let gameSectionButton of gameSectionButtons) {
-
-        gameSectionButton.addEventListener("click", function () {
-            // Identify clicked gameSectionButton
-            let choiceSelected = this.getAttribute("data-choice");
-            let userChoice = choices.find(choice => choice.name === choiceSelected);
-
-            console.log(userChoice);
-            // Write selection into player choice display
-            document.getElementById("playerChoice").innerHTML = `${choiceSelected}`;
-
-            // Generate random computer choice and write to the computer output display
-            let randomComputer = choices[Math.floor(Math.random() * choices.length)];
-            document.getElementById("computerChoice").innerHTML = `${randomComputer.name}`;
-        });
-    }
-});
-
-
-
-
+// Main game 

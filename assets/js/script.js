183512fb9dd1b2d53bf4fe0b73356cb24ab61d14
MattuW4 <>
2023-07-08 23:08:53 +0000

    Clear JS script of main game and rewrite player choice function/event listener, computer random generator/event listener, check winner function and write functions to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,73 @@
-const choices = [{
-    name: 'rock',
-    image: './assets/images/Rock.png',
-    beats: 'scissors'
-},
-{
-    name: 'paper',
-    image: './assets/images/Paper.png',
-    beats: 'rock',
-},
-{
-    name: 'scissors',
-    image: './assets/images/Scissors.png',
-    beats: 'paper'
-}
-];
+// Main game 
 
-let playRound = document.querySelector(".roundNumber");
+let buttons = document.querySelectorAll(".player-weapon-choice-button");
+let playerTotal = document.getElementById("playerScore");
+let computerTotal = document.getElementById("computerScore");
+let playerImage = document.getElementsByClassName(".hands-image");
+let choices = ["rock", "paper", "scissors"];
+
+let playerScore = 0;
+let computerScore = 0;
+let accruedRounds = 0;
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let userChoice = this.getAttribute("data-choice");
+        runGame(userChoice);
+        console.log(userChoice);
+    });
+}
+
+function runGame(userChoice) {
+
+    // playerImage.src = `assets/images/${choices[userChoice]}.png`;
+    // playerImage.alt = choices[userChoice];
+
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerChoice);
+
+    let result = checkWinner(choices[computerChoice], choices[userChoice]);
+
+    updateScores(result);
+}
+
+function checkWinner(player, computer) {
+    let arenaVictorOutput = document.querySelector(".arenaVictorOutput");
+    if (player === computer) {
+        arenaVictorOutput.textContent = "Draw!";
+    }
+    if (player == "rock") {
+        if (computer === "paper") {
+            arenaVictorOutput.textContent += "Computer won!";
+            computerScore++;
+        } else {
+            arenaVictorOutput.textContent += "Player won!";
+            playerScore++;
+        }
+    }
+    else if (player == "scissors") {
+        if (computer === "rock") {
+            arenaVictorOutput.textContent += "Computer won!";
+            computerScore++;
+        } else {
+            arenaVictorOutput.textContent += "Player won!";
+            playerScore++;
+        }
+    }
+    else if (player == "paper") {
+        if (computer === "scissors") {
+            arenaVictorOutput.textContent += "Computer won!";
+            computerScore++;
+        } else {
+            arenaVictorOutput.textContent += "Player won!";
+            playerScore++;
+        }
+    }
+}
+
+
+
+// Modal 
 
 //Variable declarations
 const modal = document.getElementById("my-modal");
@@ -35,124 +87,3 @@ function showModal() {
 function hideModal() {
     modal.style.display = "none";
 }
-// Main game 
-
-const buttons = document.querySelectorAll(".player-weapon-choice-button");
-
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let userChoice = this.getAttribute("data-choice");
-        let userOption = choices.find(choice => choice.name === userChoice);
-        console.log(userChoice);
-
-        runGame(userOption);
-    });
-}
-
-function computerRandomChoice() {
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randomChoice.name);
-    return randomChoice;
-}
-
-function runGame(userOption) {
-    let computerOption = computerRandomChoice();
-    let computerSelection = computerOption.name;
-    let userSelection = userOption.name;
-    console.log(userOption, computerOption);
-
-    if (userSelection === computerSelection) {
-        drawResult(userSelection, computerSelection);
-    } else {
-        let userWins = whoWins(userOption, computerSelection);
-
-        if (userWins) {
-            winnerUser(userSelection, computerSelection);
-            scoreUser();
-        } else {
-            winnerComputer(userSelection, computerSelection);
-            scoreComputer();
-        }
-    }
-    roundGame(userSelection, computerSelection);
-}
-
-function whoWins(selection, nameSelected) {
-    let options = selection.beats;
-    for (let i = 0; i < options.length; i++) {
-        let option = options[i];
-        if (option === nameSelected) {
-            return true;
-        }
-    }
-}
-
-function drawResult(user, computer) {
-    arenaVictorOutput.innerText = "draw";
-}
-
-function roundGame(user, computer) {
-    if (user && computer) {
-        let incrementRound = parseInt(roundNumber.innerText);
-        roundNumber.innerText = ++incrementRound;
-
-        if (incrementRound === 5) {
-            document.getElementById(roundNumber).innerText = "Game over";
-
-        }
-    }
-}
-
-function computerScore() {
-    let incrementComputerScore = parseInt(computerScore.innerText);
-    computerScore.innerText = ++incrementComputerScore;
-}
-
-function userScore() {
-    let incrementuserScore = parseInt(userScore.innerText);
-    userScore.innerText = ++incrementuserScore;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

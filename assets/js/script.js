bf172a0ba243130a6999269e1fbbd28d6e51d2f7
MattuW4 <>
2023-07-09 17:59:56 +0000

    Add function to determine victor. Add function for running end game output. Change constant variables for playerCount, computerCOunt, roundCount and roundIncrease to let variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 
 // Variables
 const game = () => {
-    const playerCount = 0;
-    const computercount = 0;
-    const roundCount = 0;
+    let playerCount = 0;
+    let computerCount = 0;
+    let roundCount = 0;
 
     const playGame = () => {
         const rockChoice = document.getElementById("rock");
@@ -15,14 +15,14 @@ const game = () => {
 
         playerChoices.forEach(choice => {
             choice.addEventListener("click", function () {
-                const roundIncrease = document.querySelector(".roundNumber");
+                let roundIncrease = document.querySelector(".roundNumber");
                 roundCount++;
-                roundIncrease.innerHTML++;
+                roundIncrease++;
 
                 const randomChoice = Math.floor(Math.random() * 3);
                 const computerSelection = computerChoices[randomChoice];
 
-                ServiceWorkerContainer(this.innerText, computerSelection);
+                victor(this.innerText, computerSelection);
 
                 if (roundCount == 5) {
                     gameOver(playerChoices, roundIncrease);
@@ -31,28 +31,102 @@ const game = () => {
         });
     };
 
+    const victor = (player, computer) => {
+        const resultOutput = document.querySelector(".arenaVictorOutput");
+        const playerScoreDisplay = document.querySelector(".playerScore");
+        const computerScoreDisplay = document.querySelector(".computerScore");
+        if (player === computer) {
+            resultOutput.textContent = "Draw";
+        } else if (player === "rock") {
+            if (computer == "paper") {
+                resultOutput.textContent = "Computer wins!";
+                computerCount++;
+                computerScoreDisplay.textContent = computerCount;
+
+            } else {
+                resultOutput.textContent = "Player wins!";
+                playerCount++;
+                playerScoreDisplay.textContent = playerCount;
+            }
+        }
+        else if (player == "scissors") {
+            if (computer == "rock") {
+                resultOutput.textContent = "Computer wins!";
+                computerCount++;
+                computerScoreDisplay.textContent = computerCount;
+            } else {
+                resultOutput.textContent = "Player wins!";
+                playerCount++;
+                playerScoreDisplay.textContent = playerCount;
+            }
+        }
+        else if (player == "paper") {
+            if (computer == "scissors") {
+                resultOutput.textContent = "Computer wins!";
+                computerCount++;
+                computerScoreDisplay.textContent = computerCount;
+            } else {
+                resultOutput.textContent = "Player wins!";
+                playerCount++;
+                playerScoreDisplay.textContent = playerCount;
+            }
+        }
+    };
+
+    const gameOver = (playerChoices, roundIncrease) => {
+        const chooseWeapon = document.querySelector(".weapon-choice-instruction");
+        const resultOutput = document.querySelector(".arenaVictorOutput");
+        const replayButton = document.querySelector(".arenaVictorOutput");
+
+        playerChoices.forEach(choice => {
+            choice.style.display = "none";
+        });
+        chooseWeapon.innerText = "Game Over!";
+        roundIncrease.style.display = "none";
+
+        if (playerCount > computerCount) {
+            resultOutput.style.fontSize = "3rem";
+            resultOutput.style.fontSize = "You are a weiner!";
+        }
+        else if (playerCount < computerCount) {
+            resultOutput.style.fontSize = "3rem";
+            resultOutput.style.fontSize = "You are a loser!";
+        } else {
+            resultOutput.style.fontSize = "3rem";
+            resultOutput.style.fontSize = "Draw! (computer is the winner really, loser...";
+        }
+        replayButton.innerText = "Replay: to and be a weiner";
+        replayButtonstyle.display = "flex";
+        replayButton.addEventListener("click", () => {
+            window.location.reload();
+        });
+    };
+    playGame();
+};
+game();
 
 
 
-    // Modal 
 
-    //Variable declarations
-    const modal = document.getElementById("my-modal");
-    const openModal = document.getElementById("open-modal-button");
-    const closeModal = document.querySelector(".return-button");
+// Modal 
 
-    /**  Modal open and hide */
-    openModal.addEventListener("click", showModal);
-    closeModal.addEventListener("click", hideModal);
+//Variable declarations
+const modal = document.getElementById("my-modal");
+const openModal = document.getElementById("open-modal-button");
+const closeModal = document.querySelector(".return-button");
 
-    /** Show modal function */
-    function showModal() {
-        modal.style.display = "block";
-    }
+/**  Modal open and hide */
+openModal.addEventListener("click", showModal);
+closeModal.addEventListener("click", hideModal);
 
-    /** Hide modal function */
-    function hideModal() {
-        modal.style.display = "none";
-    }
+/** Show modal function */
+function showModal() {
+    modal.style.display = "block";
+}
+
+/** Hide modal function */
+function hideModal() {
+    modal.style.display = "none";
+}
 
 

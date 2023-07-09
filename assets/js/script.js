6583b1a25f15a429988ec18a565b8c01aef8bb25
MattuW4 <>
2023-07-09 17:15:42 +0000

    Add game variables. Add button event listeners for player choice. Add random computer choice generator. Add function to check winner. Add function calling game end following 5 rounds.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,58 @@
 // Main game 
 
+// Variables
+const game = () => {
+    const playerCount = 0;
+    const computercount = 0;
+    const roundCount = 0;
+
+    const playGame = () => {
+        const rockChoice = document.getElementById("rock");
+        const paperChoice = document.getElementById("paper");
+        const scissorsChoice = document.getElementById("scissors");
+        const playerChoices = [rockChoice, paperChoice, scissorsChoice];
+        const computerChoices = ["rock", "paper", "scissors"];
+
+        playerChoices.forEach(choice => {
+            choice.addEventListener("click", function () {
+                const roundIncrease = document.querySelector(".roundNumber");
+                roundCount++;
+                roundIncrease.innerHTML++;
+
+                const randomChoice = Math.floor(Math.random() * 3);
+                const computerSelection = computerChoices[randomChoice];
+
+                ServiceWorkerContainer(this.innerText, computerSelection);
+
+                if (roundCount == 5) {
+                    gameOver(playerChoices, roundIncrease);
+                }
+            });
+        });
+    };
 
 
 
 
+    // Modal 
 
-// Modal 
+    //Variable declarations
+    const modal = document.getElementById("my-modal");
+    const openModal = document.getElementById("open-modal-button");
+    const closeModal = document.querySelector(".return-button");
 
-//Variable declarations
-const modal = document.getElementById("my-modal");
-const openModal = document.getElementById("open-modal-button");
-const closeModal = document.querySelector(".return-button");
+    /**  Modal open and hide */
+    openModal.addEventListener("click", showModal);
+    closeModal.addEventListener("click", hideModal);
 
-/**  Modal open and hide */
-openModal.addEventListener("click", showModal);
-closeModal.addEventListener("click", hideModal);
+    /** Show modal function */
+    function showModal() {
+        modal.style.display = "block";
+    }
 
-/** Show modal function */
-function showModal() {
-    modal.style.display = "block";
-}
+    /** Hide modal function */
+    function hideModal() {
+        modal.style.display = "none";
+    }
 
-/** Hide modal function */
-function hideModal() {
-    modal.style.display = "none";
-}
+
